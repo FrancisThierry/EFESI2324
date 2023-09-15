e84ebc716b67545fc182b72c4572f3be57b80c8a
FrancisThierry <boulangerthie@gmail.com>
2023-09-15 10:44:47 +0200

    ajout d un exemple  db first

---
+++ ConsoleAppAnimals/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppAnimals;
+
+public partial class Animal
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Species { get; set; }
+}
---
+++ ConsoleAppAnimals/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppAnimals;
+
+Console.WriteLine("Hello, World!");
+
+//Ajouter un animal dans la base
+
+DatabaseAnimalsContext context = new DatabaseAnimalsContext();
+context.Animals.Add(new Animal { Name = "Lion", Species = "Félin" });
+context.SaveChanges();
+
+foreach (var animal in context.Animals)
+{
+    Console.WriteLine(string.Format("{0} a pour espèce :    {1}", animal.Name, animal.Species));
+}
